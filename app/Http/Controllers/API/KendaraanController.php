b60b64e4e427ff7589343423d17ca17aced80c08
rafiatha84 <muhammad.19084@mhs.unesa.ac.id>
2021-12-15 02:20:38 +0700

    oke fic kkj

--- app/Http/Controllers/API/KendaraanController.php
+++ app/Http/Controllers/API/KendaraanController.php
@@ -47,7 +47,7 @@ class KendaraanController extends Controller
     public function most()
     {
         $kendaraan = Kendaraan::with('user','kategori',
-        'transaksi','ratingKendaraan')->groupBy('kategori_id')->skip(0)->take(2)->get();
+        'transaksi','ratingKendaraan')->skip(0)->take(2)->get()->unique('kategori_id');
 
         if(count([$kendaraan]) > 0){
             $response = [
