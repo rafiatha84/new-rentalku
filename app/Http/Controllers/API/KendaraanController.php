fd7911448953ff3a077139cb4253cad7df8e01b1
rafiatha84 <muhammad.19084@mhs.unesa.ac.id>
2021-12-15 02:26:58 +0700

    oke fic 2shk

--- app/Http/Controllers/API/KendaraanController.php
+++ app/Http/Controllers/API/KendaraanController.php
@@ -48,6 +48,8 @@ class KendaraanController extends Controller
     {
         $kendaraan = Kendaraan::with('user','kategori',
         'transaksi','ratingKendaraan')->skip(0)->take(2)->get()->unique('kategori_id');
+        $kendaraanArray = $kendaraan->toArray();
+        $kendaraanArray['kategori'] = $kendaraan->kategori->name;
 
         if(count([$kendaraan]) > 0){
             $response = [
