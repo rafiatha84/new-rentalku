58c669e298174c93512e6814c2bf9bf6f27af95d
rafiatha84 <muhammad.19084@mhs.unesa.ac.id>
2021-12-16 11:41:23 +0700

    oke jhdfkj 2

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -49,7 +49,7 @@ class Kendaraan extends Model
     public function avgRating()
     {
         return $this->ratingKendaraan()
-        ->selectRaw('avg(jumlah_bintang) as aggregate, kendaraan_id')
+        ->selectRaw('avg(jumlah_bintang)')
         ->groupBy('kendaraan_id');
     }
 
