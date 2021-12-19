08838f008e56bb42bccfb7d53242a82e840eda29
rafiatha84 <muhammad.19084@mhs.unesa.ac.id>
2021-12-19 23:10:20 +0700

    oke

--- app/Http/Controllers/User/UserKendaraanController.php
+++ app/Http/Controllers/User/UserKendaraanController.php
@@ -44,7 +44,7 @@ class UserKendaraanController extends Controller
             }
         }
 
-        $kendaraans = Kendaraan::with('kategori')->withAvg('ratingKendaraan', 'jumlah_bintang')->whereRaw("")->paginate(6);
+        $kendaraans = Kendaraan::with('kategori')->withAvg('ratingKendaraan', 'jumlah_bintang')->whereRaw("1 = 1")->paginate(6);
         dd($kendaraans);
         if(isset($request->q) && $request->q != ""){
         }else{
