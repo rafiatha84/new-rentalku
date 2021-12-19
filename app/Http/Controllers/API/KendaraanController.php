11284fce8a1e8c539ecfa8dcd12ca6b14c769049
rafiatha84 <muhammad.19084@mhs.unesa.ac.id>
2021-12-20 00:24:03 +0700

    oke

--- app/Http/Controllers/API/KendaraanController.php
+++ app/Http/Controllers/API/KendaraanController.php
@@ -114,7 +114,7 @@ class KendaraanController extends Controller
                 "errors" => null,
                 "content" => $kendaraans,
             ];
-            return response()->json($response, 404,[
+            return response()->json($response, 200,[
                 'Content-Type' => 'application/json',
                 'Charset' => 'utf-8'
             ]);
