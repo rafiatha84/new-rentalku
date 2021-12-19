515aac8f925b276babaf080b05fb4f39f050a62e
rafiatha84 <muhammad.19084@mhs.unesa.ac.id>
2021-12-19 20:31:28 +0700

    oke

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,7 +54,7 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('user.dashboard') }}">Dashboard</a>
+                                    <a class="nav-link" href="{{ route('user.dashboard') }}">Dasbor</a>
                                 </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
